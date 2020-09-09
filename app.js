0a5e27f96b699f842564d51a62e9ec94c576ff1f
TechFallMacBook <techfallmacbook@MacBook-Pro.home>
2020-09-08 20:45:23 -0400

    not finished

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 $(document).ready(()=>{
     $('#keyboard-upper-container').hide()
 
+    let currentSentence = 0
+    let currentLetter = 0
+    let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+
     $(document).keydown((e)=>{
         if (e.keyCode == 16){
             $('#keyboard-upper-container').show()
@@ -19,12 +23,14 @@ $(document).ready(()=>{
         setTimeout(()=>{
             $(`#${e.keyCode}`).css('backgroundColor','')
         },200)
-        
+        $($letter).appendTo($('#target-letter'))
+        currentLetter++
     })
+    
+    
 
-    /*
-    $(document).keyup((e)=>{
-        $(`#${e.keyCode}`).css('backgroundColor','white')
-    })
-    */
+    let $sentence = `<p>${sentences[currentSentence]}</p>`
+    let $letter = `<p>${sentences[currentSentence][currentLetter]}</p>`
+    $($sentence).appendTo($('#sentence'))
+
 })
